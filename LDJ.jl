da457d69c16992caefc44907826a422b76c3fdca
untoreh <francesco.giannelli@yahoo.it>
2021-09-28 16:39:36 +0200

    init

---
+++ LDJ.jl
@@ -0,0 +1,581 @@
+module LDJ
+
+# include("Content.jl")
+# using .Content
+
+using JSON
+using Dates: Date, now, year, DateTime
+
+const DATE_FORMAT = "mm/dd/yyyy"
+const EMPTY_DATE = Date("0001-01-01")
+
+macro unimp(fname)
+    quote
+        function $(esc(fname))()
+            throw("unimplemented")
+        end
+    end
+end
+
+@doc "Set properties (a (k, v) vector) to data (an dict)."
+macro setprops!()
+    quote
+        pr = $(esc(:props))
+        data = $(esc(:data))
+        if !isempty(pr)
+            for (p, v) in pr
+	            data[p] = v
+            end
+        end
+        data
+    end
+end
+
+macro Name(arg)
+    x = string(arg)
+    quote
+	    Symbol($x)
+    end
+end
+
+macro setargs!(args...)
+    quote
+        data = $(esc(:data))
+        for (k, v) in $(esc(args))
+            if !isempty(v)
+                data[k] = v
+            end
+        end
+    end
+end
+
+
+## LD+JSON functions
+@inline function schema()
+    "@context" => "https://schema.org"
+end
+
+@inline function wrap_ldj(data::IdDict, wrap=true)
+    "<script type=\"application/ld+json\">$(JSON.json(data))</script>"
+end
+
+@doc "https://schema.org/WebSite"
+function website(url, author, year)
+    IdDict(
+        "@context" => "https://schema.org/",
+        "@type" => "WebSite",
+        "@id" => url,
+        "url" => url,
+        "copyrightHolder" => author,
+        "copyrightYear" => year,
+    )
+end
+
+@doc "https://schema.org/SearchAction
+`parts` are url parts (scheme, host, port, path, query, fragment) to merge with the url string. It has
+to contain a parameter which value is `{input}`."
+function search(url::AbstractString; parts=NamedTuple(), maxlength=100)
+    @assert "{input}" in values(parts.query)
+    uri = URI(url)
+    IdDict(
+        "potentialAction" => IdDict(
+            "@type" => "SearchAction",
+            "target" => URI(uri; parts...),
+            "query" => "required",
+            "query-input" => "required maxlength=$(maxlength) name=input",
+            "actionStatus" => "https://schema.org/PotentialActionStatus",
+        ),
+    )
+end
+
+@doc "https://schema.org/Place"
+function place(place="homeLocation"; country="", region="", props=[])
+    data = IdDict(
+        place => IdDict(
+            "@type" => "https://schema.org/Place",
+            "addressCountry" => locvar(:country),
+            "addressRegion" => locvar(:region),
+        ),
+    )
+    if !isempty(country)
+        data["addressCountry"] = country
+    end
+    if !isempty(region)
+        data["addressRegion"] = region
+    end
+    @setprops!
+    data
+end
+
+@doc "Convenience function for authors.
+Requires at least `name` and `email`."
+function author(entity="Person"; name, email="", desc="", image="", sameAs="")
+    IdDict(
+        "@type" => "https://schema.org/$(entity)",
+        "name" => name,
+        "email" => email,
+    )
+    @setargs! ("image" => image, "description" => desc, "sameAs" => sameAs)
+end
+
+@doc "Currently same as `author`"
+function publisher(args...; kwargs...)
+	author(args; kwargs...)
+end
+
+@doc "Create a list of Language types"
+function languages(langs)
+    [IdDict("@type" => "Language", "name" => l) for l in langs]
+end
+
+function webpage(;id, title, url, mtime, selector, description, keywords, name="", headline="",
+                 image="", entity="Article", status="Published",lang="english", mentions=[],
+                 access_mode=["textual", "visual"], access_sufficient=[], access_summary="",
+                 created="", published="", props=[]),
+	IdDict(
+        "@context" => "https://schema.org",
+        "@type" => "https://schema.org/WebPage",
+        "@id" => id,
+        "url" => url,
+        "lastReviewed" => mtime,
+        "mainEntityOfPage" => IdDict(
+            "@type" => entity,
+            "@id" => url
+        ),
+        "mainContentOfPage" =>
+            IdDict("@type" => "WebPageElement", "cssSelector" => selector),
+        "accessMode" => access_mode,
+        "accessModeSufficient" => IdDict(
+            "@type" => "itemList",
+            "itemListElement" => isempty(access_sufficient) ? access_mode : access_sufficient,
+        ),
+        "creativeWorkStatus" => status,
+        "dateModified" => mtime_raw,
+        "dateCreated" => isempty(created) ? mtime,
+        "datePublished" => isempty(published) ? mtime,
+        "name" => isempty(name) ? title : name,
+        "description" => description,
+        "keywords" => keywords
+    )
+    setargs!("inLanguage" => lang, "accessibilitySummary" => access_summary,
+             "audience" => audience, "headline" => headline, "image" => image,
+             "mentions" => mention
+             )
+    @setprops!
+end
+
+@doc "file path must be relative to the project directory, assumes the published website is under '__site/'"
+function translation(;src_url, trg_url, lang, title, mtime, selector, description, keywords, mentions="",
+                     translator_name="Google", translator_url="https://translate.google.com/")
+
+    data = webpage(;id=trg_url, title, url=trg_url, mtime, selector, description,
+                   keywords, mentions, lang)
+    data["translator" ] = IdDict("@type" => "https://schema.org/Organization",
+                               "name" => translator_name,
+                               "url" => translator_url)
+    data["translationOfWork"] = IdDict("@id" => src_url)
+    data
+end
+
+@doc """Take a list of (name, link) tuples and returns a breadcrumb
+definition with hierarchy from top to bottom"""
+function breadcrumbs(items)
+    IdDict(
+        "@type" => "BreadcrumbList",
+        "itemListElement" => [
+            IdDict(
+                "@type" => "ListItem",
+                "position" => n,
+                "name" => name,
+                "item" => item
+            ) for (n, (name, item)) in enumerate(items)
+                ]
+    )
+end
+
+Book = @NamedTuple begin
+	name::String
+    author::String
+    url::String
+    sameas::String
+    tags::Vector{String}
+    comments::String
+end
+
+function book(name, author, url, tags, sameas)
+    book = IdDict(                schema(),
+                                  "@type" => "Book",
+                                  "@id" => url,
+                                  "url" => url,
+                                  "urlTemplate" => url,
+                                  "name" => name,
+                                  "author" => IdDict(
+                                      "@type" => "Person",
+                                      "name" => author
+                                  ),
+                                  "sameAs" => sameas )
+    !isempty(url) && begin
+	    book["url"] = url
+        book["@id"] = url
+        book["urlTemplate"] = url
+    end
+    book
+end
+
+function bookfeed(books; props=[])
+    data = IdDict(
+        schema(),
+        "@type" => "DataFeed",
+        "dataFeedElement" => [book(b...) for b in books],)
+    @setprops!
+end
+
+function event_status(status)
+    let schema = "https://schema.org/Event"
+        if status === "cancelled"
+            schema * "Cancelled"
+        elseif status === "moved"
+            schema * "MovedOnline"
+        elseif status === "postponed"
+            schema * "Postponed"
+        elseif status === "rescheduled"
+            schema * "Rescheduled"
+        else
+            schema * "Scheduled"
+        end
+    end
+end
+
+function online_event(;name, start_date, end_date, url, image=[], desc="",
+                      status="EventScheduled", prev_date="",
+                      perf=IdDict(), org=IdDict(), offers=IdDict())
+	IdDict(
+        schema(),
+        "@type" => "Event",
+        "name" => name,
+        "startDate" => start_date,
+        "endDate" => end_date,
+        "previousStartDate" => prev_date,
+        "eventStatus" => event_status(status),
+        "eventAttendanceMode" => "https://schema.org/OnlineEventAttendanceMode",
+        "location" => IdDict(
+            "@type" => "VirtualLocation",
+            "url" => url
+        ),
+        "image" => image,
+        "description" => desc,
+        "offers" => offers,
+        "performer" => perf,
+        "organizer" => org)
+end
+
+function license(name="")
+	if name === "mit"
+        "https://en.wikipedia.org/wiki/MIT_License"
+    elseif name === "apache"
+        "https://en.wikipedia.org/wiki/Apache_License"
+    elseif name === "gpl" || name === "gplv3"
+        "https://www.gnu.org/licenses/gpl-3.0.html"
+    elseif name === "gplv2"
+        "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html"
+    elseif name === "sol"
+        "https://wiki.p2pfoundation.net/Copysol_License"
+    elseif name === "crypto" || name === "cal"
+        "https://raw.githubusercontent.com/holochain/cryptographic-autonomy-license/master/README.md"
+    else
+        "https://creativecommons.org/publicdomain/zero/1.0/"
+    end
+end
+
+function orgschema(name, url, contact="", tel="", email="", sameas="")
+    IdDict(
+        "@type" => "Organization",
+        "name" => name,
+        "url" => url,
+        "sameAs" => sameas,
+        "contactPoint" => IdDict(
+            "@type" => "ContactPoint",
+            "contactType" => contact,
+            "telephone" => tel,
+            "email" => email,))
+end
+
+function coverage(start_date, end_date="")
+	start_date * "/" * (isempty(end_date) ? ".." : end_date)
+end
+
+function place_schema(coords="")
+    IdDict(
+        "@type" => "Place",
+        "geo" => IdDict(
+            "@type" => "GeoShape",
+            "box" => coords
+        )
+    )
+end
+
+@doc "dist is a tuple of (format, url) for content format and download link"
+function dataset(;name, url, desc="", sameas="", id="",
+                 keywords=[], parts=[], license="", access=true,
+                 creator=IdDict(), funder=IdDict(), catalog="", dist=[],
+                 start_date="", end_date="", coords="")
+    IdDict(
+        schema(),
+        "@type" => "Dataset",
+        "name" => name,
+        "url" => url,
+        "description" => desc,
+        "sameAs" => sameas,
+        "identifier" => isempty(id) ? url : id,
+        "keywords" => keywords,
+        "hasPart" => parts,
+        "license" => license,
+        "isAccessibleForFree" => access,
+        "creator" => creator,
+        "funder" => funder,
+        "includedInDataCatalog" => IdDict(
+            "@type" => "DataCatalog",
+            "name" => catalog
+        ),
+        "distribution" => [
+            IdDict(
+                "@type" => "DataDownload",
+                "encodingFormat" => f,
+                "contentUrl" => d
+            ) for (f, d) in dist
+                ],
+        "temporalCoverage" => isempty(start_date) ? start_date : coverage(start_date, end_date),
+        "spatialCoverage" => place_schema(coords)
+    )
+end
+
+
+function faqschema(faqs)
+    IdDict(
+        schema(),
+        "@type" => "FAQPage",
+        "mainEntity" => [
+            IdDict(
+                "@type" => "Question",
+                "name" => question,
+                "acceptedAnswer" => IdDict(
+                    "@type" => "Answer",
+                    "text" => answer
+                )
+            ) for (question, answer) in faqs])
+end
+
+@doc "estimatedCost, MonetaryAmount (monetary) or Text"
+function cost(type; currency="USD", value="0")
+    if type === "monetary"
+	    IdDict(
+            "@type" => "MonetaryAmount",
+            "currency" => currency,
+            "value" => value
+        )
+    else
+        type
+    end
+end
+
+function image(url; width="", height="", license=(license="", acquire=""))
+	IdDict(
+        schema(),
+        "@type" => "ImageObject",
+        "url" => url,
+        "width" => width,
+        "height" => height,
+        "license" => license.license,
+        "acquireLicensePage" => license.acquire)
+end
+
+@doc "create an HowToSupply, HowToTool or HowToStep"
+function howtoitem(name, type="supply"; props=[])
+    if type === "supply"
+        tp = "HowToSupply"
+    elseif type === "item"
+        tp = "HowToItem"
+    elseif type === "direction"
+        tp = "HowToDirection"
+    elseif type === "tip"
+        tp = "HowToTip"
+    else
+        tp = "HowToStep"
+    end
+    data = IdDict(
+        "@type" => tp,
+        "name" => name
+    )
+    @setprops!
+end
+
+function howto(;name, desc="", image=IdDict(),
+               cost=(currency="USD", value=0), supply=[],
+               tool=[], step=[], totaltime="")
+	IdDict(
+        schema(),
+        "@type" => "HowTo",
+        "name" => name,
+        "description" => desc,
+        "image" => image,
+        "estimatedCost" => cost("monetary"; cost...),
+        "supply" => supply,
+        "tool" => tool,
+        "step" => step,
+        # https://en.wikipedia.org/wiki/ISO_8601#Durations
+        "totalTime" => "")
+end
+
+function logo(;type="Organization", url, logo, props=[])
+    data = IdDict(
+        schema(),
+        "@type" => type,
+        "url" => url,
+        "logo" => logo
+    )
+    @setprops!
+end
+
+function ratingprop(value, best, count)
+    "aggregateRating" => IdDict(
+        "@type" => "AggregateRating",
+        "ratingValue" => value,
+        "bestRating" => best,
+        "ratingCount" => count)
+end
+
+function movie(;url, name, image="", created="", director="", rating="", review_author="", review="", props=[])
+    data = IdDict(
+        "@type" => "Movie",
+        "url" => url,
+        "name" => name,
+        "image" => image,
+        "dateCreated" => created,
+        "director" => IdDict(
+            "@type" => "Person",
+            "name" => director,),
+        "review" => IdDict(
+            "@type" => "Review",
+            "reviewRating" => IdDict(
+                "@type" => "Rating",
+                "ratingValue" => rating,),
+            "author" => IdDict(
+                "type" => "Person",
+                "name" => review_author
+            ),
+            "reviewBody" => review,))
+    @setprops!
+end
+
+function itemslist(items)
+    IdDict(
+        schema(),
+        "@type" => "ItemList",
+        "itemListElement" => items
+    )
+end
+
+function review(;name, rating="", author="", review="", org=[],
+                item_props=[], props=[])
+    data = IdDict(
+        schema(),
+        "@type" => "Review",
+        "itemReviewed" => IdDict(p => v for (p, v) in item_props) ,
+        "reviewRating" => IdDict(
+            "@type" => "Rating",
+            "ratingvalue" =>  rating,
+        ),
+        "name" => name,
+        "author" => IdDict(
+            "@type" => "Person",
+            "name" => author
+        ),
+        "reviewBody" => review,
+        "publisher" => orgschema(org...)
+    )
+    @setprops!
+end
+
+function searchaction(;url, template, query, props=[])
+    data = IdDict(
+        schema(),
+        "@type" => "WebSite",
+        "url" => url,
+        "potentialAction" => IdDict(
+            "@type" => "SearchAction",
+            "target" => IdDict(
+                "@type" => "EntryPoint",
+                "urlTemplate" => template,),
+            "query-input" => "required " * query))
+    @setprops!
+end
+
+function speakable(;name, url, css::AbstractVector)
+    IdDict(
+        schema(),
+        "@type" => "WebPage",
+        "name" => name,
+        "speakable" => IdDict(
+            "@type" => "SpeakableSpecification",
+            "cssSelector" => css
+        ),
+        "url" => url
+    )
+end
+
+function pubevents(events)
+    [IdDict(
+        "@type" => "BroadcastEvent",
+        "isLiveBroadcast" => true,
+        "startDate" => start_date,
+        "endDate" => end_date) for (start_date, end_date) in events]
+end
+
+function video(;name, url, desc="", duration="", embed="",
+               expire="", regions="", views, thumbnail="", date="", pubevents=[])
+    IdDict(
+        schema(),
+        "@type" => "VideoObject",
+        "contentURL" => url,
+        "description" => desc,
+        "duration" => duration,
+        "embedURL" => embed,
+        "expires" => expire,
+        "regionsAllowed" => regions,
+        "interactionStatistic" => IdDict(
+            "@type" => "InteractionCounter",
+            "interactionType" => IdDict("@type" => "WatchAction"),
+            "userInteractionCount" => views,
+        ),
+        "name" => name,
+        "thumbnailUrl" => thumbnail,
+        "uploadDate" => date,
+        "publication" => pubevents
+    )
+end
+
+@unimp jobtraining
+
+@unimp jobposting
+
+@unimp business
+
+@unimp factcheck
+
+@unimp mathsolver
+
+@unimp practiceproblems
+
+@unimp product
+
+@unimp qapage
+
+@unimp recipe
+
+@unimp softwareapp
+
+@unimp subscription
+
+include("LDJFranklin.jl")
+
+end
---
+++ LDJFranklin.jl
@@ -0,0 +1,197 @@
+module LDJFranklin
+
+using Franklin: globvar, locvar, pagevar
+using FranklinUtils
+using Conda
+using JSON
+using IterTools: chain
+using DataStructures: OrderedDict
+using Base.Unicode: titlecase
+
+using HTTP: get
+using URIs: URI
+import Base.convert
+
+const BOOKS = []
+
+export hfun_ldj_author, hfun_ldj_publisher, hfun_ldj_place, hfun_ldj_search, hfun_ldj_website, hfun_insert_library,
+    hfun_ldj_book, hfun_ldj_crumbs, hfun_ldj_webpage
+
+@memoize function hfun_ldj_website(_="")
+    website(locvar(:website_url), locvar(:author), year(now())) |>
+        wrap_ldj
+end
+
+@memoize function hfun_ldj_search(_="")
+    search(locvar(:website_url);
+           (path="/search", query=(q="{query}",))) |> wrap_ldj
+end
+
+@memoize function hfun_ldj_place(_="")
+    place(;country=locvar(:country), region=locvar(:region))|> wrap_ldj
+end
+
+@memoize function hfun_ldj_author(_=""; wrap=true)
+    author(;name=locvar(:author),
+           image=locvar(:author_image),
+           email=locvar(:email),
+           description=isdefined( @__MODULE__, :author_bio) ? author_bio() : "",
+           sameAs=[locvar(:github), locvar(:twitter)]) |>
+               x -> wrap_ldj(x, wrap)
+end
+
+@memoize function hfun_ldj_publisher(_=""; wrap=true)
+    hfun_ldj_author(_; wrap)
+end
+
+@memoize function get_languages()
+    languages((for (lang, _) in locvar(:languages)))
+end
+
+function hfun_ldj_webpage()
+    webpage(id=locvar(:fd_full_url),
+            title=locvar(:title),
+            url=locvar(:fd_full_url),
+            mtime=locvar(:fd_mtime_raw),
+            selector= ".franklin-content",
+            description=locvar(:rss_description),
+            keywords=locvar(:tags),
+            access_mode=locvar(:accessMode),
+            access_sufficient=locvar(:accessModeSufficient),
+            access_summary="Visual elements are tentatively described.",
+            audience="cool people",
+            mentions=locvar(:mentions),
+            image=locvar(:images),
+            lang=globvar(:lang),
+            props=["availableLanguage" => get_languages(),
+                   "author" => hfun_ldj_author(;wrap=false),
+                   "publisher" => hfun_ldj_publisher(;wrap=false)]) |> wrap_ldj
+end
+
+
+@doc "file path must be relative to the project directory, assumes the published website is under '__site/'"
+function ldj_trans(file_path, src_url, trg_url, lang)
+    translation(;src_url, trg_url, lang,
+                title=pagevar(file_path, :title),
+                mtime=pagevar(file_path, :fd_mtime_raw),
+                selector=".franklin-content",
+                description=pagevar(file_path, :rss_description),
+                keywords=pagevar(file_path, :tags),
+                mentions=pagevar(file_path, :mentions),
+                image=pagevar(file_path, :images),
+                headline=pagevar(file_path, :title))
+end
+
+@doc "create breadcrumbs for posts"
+function hfun_ldj_crumbs()
+    post_crumbs() |> breadcrumbs
+end
+
+@doc "create a book structure"
+function hfun_ldj_book(args...; kwargs...)
+	book(args...; kwargs...)
+end
+
+@inline function get_date()
+	let d = fr.locvar(:date)
+        if d === EMPTY_DATE
+            DateTime(year(now), 1, 1)
+        else
+            DateTime(d, DATE_FORMAT)
+        end
+    end
+end
+
+# function lx_book(name, author, url, sameas="", _)
+function lx_book(com, _)
+    args = lxproc(com)
+    name, author, url, tags, comments  = strip.(args) |>
+        x -> split(x, r"; ?")
+    # name, author, url = split(args, "\" \"")
+    push!(BOOKS, (name=name, author=author, url=url, tags=tags, comments=comments, sameas=""))
+    "[" * name * "](" * url * ")"
+end
+
+
+convert(::Type{Symbol}, s::String) = Symbol(s)
+@doc "Fetch books list from a calibre content server"
+function calibredb_books_server(server="http://localhost:8099";
+                                query=Dict("library_id" => "",
+                                           "num" => typemax(Int32)))
+    get(server * "/interface-data/books-init";  query) |>
+        res -> String(res.body) |>
+        JSON.parse |>
+        r -> r["metadata"]
+end
+
+@doc "generate LDJ data from a list of books"
+function hfun_ldj_library()
+    empty!(BOOKS)
+    if locvar(:fd_rpath) === "reads/index.md"
+        # add calibre books to library
+        calibre_books = calibredb_books_server(locvar(:calibre_server);
+                                               query=Dict("library_id" => locvar(:calibre_library),
+                                                          "num" => 10000))
+        for (_, book) in calibre_books
+            push!(BOOKS, (name=book["title"], author=book["author_sort"],
+                          url="", tags=book["tags"], sameas=""))
+        end
+        bookfeed(BOOKS) |> wrap_ldj
+    else
+        ""
+    end
+end
+
+@doc "create html lists by grouping books based on their tags"
+function hfun_insert_library(groups=[])
+    c = IOBuffer()
+    if isempty(groups)
+        lists = Dict(["all" => Vector{String}()])
+        setgroup! = book -> begin
+            "read" ∈ book.tags && push!(lists["all"], book.name)
+        end
+    else
+        lists = OrderedDict([g => [] for g in groups])
+        setgroup! = book -> begin
+            for g in groups
+                if "read" ∈ book.tags && g ∈ book.tags
+                    push!(lists[g], book)
+                end
+            end
+        end
+    end
+    for book in BOOKS
+        setgroup!(book)
+    end
+    write(c, "<div id=\"library\">")
+    for (group_name, group_list) in lists
+        if length(group_list) > 0
+            write(c, "<h2>$(titlecase(group_name))</h2>")
+            write(c, "<ul class=\"$(group_name)-books\">")
+            for book in group_list
+                write(c, "<li class=\"book-entry\">")
+                println(c, book.name, "<div class=\"book-author\"> - ", book.author,"<div>")
+                write(c, "</li>")
+            end
+            write(c, "</ul>")
+        end
+    end
+    write(c, "</div>")
+    ret = String(take!(c))
+    close(c)
+    ret
+end
+
+# function calibre_books_cli(library="http://localhost:8099", fields="authors,isbn,publisher,tags,title")
+#     @assert !isnothing(Sys.which("calibredb"))
+#     # since calibre is a python package, and is installed system-wide,
+#     # make sure we are not using julia python env
+#     let pythonpath = ENV["PYTHONPATH"]
+#         delete!(ENV, "PYTHONPATH")
+#         l = read(`calibredb --with-library $library list --for-machine -f $fields`, String)
+#         ENV["PYTHONPATH"] = pythonpath
+#         l |> JSON.parse
+#     end
+# end
+
+end
